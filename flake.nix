a4da6f29f23a347b03f034ffcacea3183c4d5e36
Marcel Müller <neikos@neikos.email>
2022-05-22 11:15:48 +0200

    Added rust-overlay for better control of versions

--- flake.nix
+++ flake.nix
@@ -10,18 +10,34 @@
     };
 
     flake-utils.url = "github:numtide/flake-utils";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
   };
 
-  outputs = { self, nixpkgs, crane, flake-utils, ... }:
+  outputs = { self, nixpkgs, crane, flake-utils, rust-overlay, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
+          overlays = [ (import rust-overlay) ];
         };
         inherit (pkgs) lib;
 
-        craneLib = crane.lib.${system};
         src = ./.;
+
+        rustToolchain = pkgs.rust-bin.stable.latest.default;
+
+        craneLib = (crane.mkLib pkgs).overrideScope' (final: prev: {
+          rustc = rustToolchain;
+          cargo = rustToolchain;
+          rustfmt = rustToolchain;
+        });
 
         # Build *just* the cargo dependencies, so we can reuse
         # all of that work (e.g. via cachix) when running in CI
@@ -73,8 +89,8 @@
 
           # Extra inputs can be added here
           nativeBuildInputs = with pkgs; [
-            cargo
-            rustc
+            rustToolchain
+            cargo-tarpaulin
           ];
         };
       });
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+#[cfg(not(tarpaulin_include))]
 fn main() {
     println!("Hello, world!");
+}
+
+fn this_function_is_not_tested(input: u32) {
+    if input == 2 {
+        println!("It's a two!");
+    } else {
+        println!("It's a {}", input);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::this_function_is_not_tested;
+
+    #[test]
+    fn check_function() {
+        this_function_is_not_tested(123);
+        this_function_is_not_tested(2);
+    }
 }
